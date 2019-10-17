b932670e5c7a5914da813fc51bc363444544e346
Alex Dekau <alex.dekau@tylertech.com>
2019-10-17 17:23:18 -0400

    Try to make type completion on attributes work

--- src/react-web-component.interface.ts
+++ src/react-web-component.interface.ts
@@ -6,3 +6,9 @@ export interface IReactWebComponent<TProp = any> extends Partial<Function> {
   onDestroy?: () => void;
   attributeChanged?: (name: string, oldValue: unknown, newValue: unknown) => void;
 }
+
+type Merge<A, B> = {
+  [K in keyof A]: K extends keyof B ? B[K] : A[K]
+} & B;
+
+export type ReactWebComponent<TProp = any> = Merge<IReactWebComponent<TProp>, TProp>;
